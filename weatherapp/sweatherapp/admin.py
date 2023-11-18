537e6bcff476015ced11f37d226f318296a06cd9
dddddddj <djukkum@gmail.com>
2023-11-19 00:57:53 +0200

    Fixed views, urls and project structure

--- weatherapp/sweatherapp/admin.py
+++ weatherapp/sweatherapp/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import WeatherData, News, CustomUser, UserPreference, FavouriteCity, ForecastData
+from .models import WeatherData, WeatherNews, CustomUser, FavouriteCity, ForecastData
 
 admin.site.register(WeatherData)
 admin.site.register(CustomUser)
-admin.site.register(News)
-admin.site.register(UserPreference)
+admin.site.register(WeatherNews)
 admin.site.register(FavouriteCity)
 admin.site.register(ForecastData)
