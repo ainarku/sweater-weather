732378c54c315318528d4972e5636b2f778f4ece
Ainar Kuuskmann <ainar.kuuskmann@hotmail.com>
2023-11-19 14:14:44 +0200

    Remove WeatherNews and News

--- weatherapp/sweatherapp/admin.py
+++ weatherapp/sweatherapp/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import WeatherData, WeatherNews, CustomUser, FavouriteCity, ForecastData
+from .models import WeatherData, CustomUser, FavouriteCity, ForecastData
 
 admin.site.register(WeatherData)
 admin.site.register(CustomUser)
-admin.site.register(WeatherNews)
 admin.site.register(FavouriteCity)
 admin.site.register(ForecastData)
--- weatherapp/sweatherapp/utils.py
+++ weatherapp/sweatherapp/utils.py
@@ -1,6 +1,6 @@
 def kelvin_to_celsius(kelvin):
     return kelvin - 273.15
 
-def kelvin_to_fahrenheit(kelvin):
-    return (kelvin - 273.15) * 9/5 + 32
 
+def kelvin_to_fahrenheit(kelvin):
+    return (kelvin - 273.15) * 9 / 5 + 32
